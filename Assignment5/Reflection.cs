ce6a06d8e0c765fd5e8f2a9e2527630640fbce1a
RaphaelE939 <raphaelE939@stemcollegiate.ca>
2025-04-24 11:06:01 -0600

    Create Reflection.cs

---
+++ Assignment5/Reflection.cs
@@ -0,0 +1,6 @@
+// The character I made was a chill guy and I created him because I am a chill guy. I made my backround a building with a sun set in the bacground because it's chill. I made the platform street lights because it matches the building's in the bacground.
+// I used:
+pixelart.com // to make my character, background, and obstacles because I wanted it to be pixelart.
+// I exported the assets into Unity by downloading them as .PNG file.
+// While trying to make import the assets into Unity, I had a problem at first where the camera symbol wasn't showing, and then my character eith deleted itself or didn't move.
+// Eventually, after a lot of trial and error, I got my charater on the screen and made it move.
